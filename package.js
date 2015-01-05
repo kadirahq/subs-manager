b532b5c8c58806fbadf509c86f34181b3697d0a0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-05 12:59:33 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Subscriptions Manager for Meteor",
-  "version": "1.2.3",
+  "version": "1.2.4",
   "git": "https://github.com/meteorhacks/subs-manager.git",
   "name": "meteorhacks:subs-manager"
 });
