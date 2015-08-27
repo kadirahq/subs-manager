89d415b63ae93147d3b16e64d789b665479708de
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-08-27 11:47:22 +0530

    v1.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+### v1.6.0
+
+* Add gloabl `.ready()` api where we check the ready status of the whole subsManager at anytime.
+
 ### v1.4.0
 
 IE8 Compatibility
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Subscriptions Manager for Meteor",
-  "version": "1.5.2",
+  "version": "1.6.0",
   "git": "https://github.com/meteorhacks/subs-manager.git",
   "name": "meteorhacks:subs-manager"
 });
